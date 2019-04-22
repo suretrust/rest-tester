cb3cf6e95372a8c4e3752d13b96ae47fbdb9ed23
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-04-22 14:01:01 +0200

    non GET requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,28 @@
 class UsersController < ApplicationController
   def index
-    @name = "I am the Index action!"
+    @name = 'I am the Index action!'
+  end
+
+  def new
+
+  end
+
+  def show
   end
 
   def create
-    @name = "I am the Create action!"
+    
   end
 
   def edit
-    @name = "I am the Edit action!"
+    @name = 'I am the Edit action!'
+  end
+
+  def update
+
   end
 
   def destroy
-    @name = "I am the Destroy action!"
+    @name = 'I am the Destroy action!'
   end
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,6 +1,10 @@
 require 'rest-client'
 
-print "Enter any web URL of your choice: "
+print 'Enter any web URL of your choice: '
 url = gets.chomp
+# print 'Enter the id number'
+# ID = gets.chomp.to_i
 
-puts RestClient.get(url)
+# puts RestClient.get(url, params: { id: ID })
+#puts RestClient.get(url)
+puts RestClient.post(url,'1')
